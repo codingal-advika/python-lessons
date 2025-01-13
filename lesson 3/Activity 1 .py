3b8fc6c922ded9c2a6ea194c89070e30ce2b7806
Advika <advika-vk@outlook.com>
2025-01-13 17:58:00 +0000

    initial commit

---
+++ lesson 3/Activity 1 .py
@@ -0,0 +1,13 @@
+quit1=56
+print(type(quit1))
+ad2=65.5
+print(type(ad2))
+na3=True
+print(type(na3))
+vj4='hello my name is advika'
+print(type(vj4))
+
+#string operations
+fn='Advika '
+ln='Vijay kumar '
+print('hello ',fn+ln)
