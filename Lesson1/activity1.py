1d14202cf6faa94109319cad2d398999eda2bf7e
Advika <advika-vk@outlook.com>
2025-01-13 16:58:01 +0000

    Lesson1 commit

---
+++ Lesson1/activity1.py
@@ -0,0 +1,6 @@
+print('Hello my name is Advika!')
+print('Today I am going to ask you a maths question.Get ready!')
+print('When you multiple 3 and 15,you get an answer.',3*15)
+print('Well done your amazing at this',end='!')
+name='Advika'
+print(name)
