8e4112ab25a32bbe16611a252677688ec7b4bdea
Advika <advika-vk@outlook.com>
2025-01-16 17:49:30 +0000

    initial commit

---
+++ Lesson4/Activity.py
@@ -0,0 +1,16 @@
+t1=98
+t2=94
+t3=41
+t4=96
+t5=11
+avg=(t1+t2+t3+t4+t5)/5
+print('the average height of the trees is equal to ',avg)
+
+#percentage calaculation
+math=int(input('enter your math marks'))
+english=int(input('enter your marks for english'))
+hindi=int(input('enter your marks for hindi'))
+sum=math+english+hindi
+print('sum of the subjects is ',sum)
+perc=(sum/300)*100
+print('your percentage is ',perc)
