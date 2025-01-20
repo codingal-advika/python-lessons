846060b14453577eeedddbf2a916b42bbb8804ca
Advika <advika-vk@outlook.com>
2025-01-20 17:48:55 +0000

    initial commit

---
+++ Lesson 5/Lesson 5.py
@@ -0,0 +1,21 @@
+beach= 'sunny'
+if beach=='sunny':
+    print('Then go to the beach')
+    
+num= 56
+if num> 0:
+    print('It is a positive number')
+
+a= -45
+if a<0:
+    print('It is a negative number')
+
+#if-else
+
+cp=int(input('Enter cost price in pounds '))
+sp=int(input('Enter selling price in pounds '))
+
+if cp< sp :
+    print('You have gained a profit of ', sp-cp)
+else:
+    print('You have gained a loss of ',cp-sp)
