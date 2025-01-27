6c4145ae572efbe9ced2bfa2f886a353a736d952
Advika <advika-vk@outlook.com>
2025-01-27 18:04:58 +0000

    commiting python operators

---
+++ Lesson 7 - Python operators/Python Operators.py
@@ -0,0 +1,13 @@
+A = int(input('Enter value'))
+if(type(A) is int):
+    print('It is an number')
+else:
+    print('It is not a number')
+
+#Is not operator
+
+B = input('Enter a number')
+if(type(B) is not float):
+    print('True')
+else:
+    print('False')
