d8e5a025aa58abf3b4fd5ab3071beded4d22406b
Advika <advika-vk@outlook.com>
2025-01-29 18:47:54 +0000

    Python challenges

---
+++ Lesson 8 - Python challenges/Pythonchallenges.py
@@ -0,0 +1,16 @@
+'''A= int(input('Enter in a number'))
+B= int(input('Enter in a number'))
+C= int(input('Enter in a number'))
+D=int(input('Enter in a number'))
+E= int(input('Enter in a number'))
+Ans = (A+B)*D**E/E
+print(Ans)
+print('Well done')'''
+
+
+name = input('enter name')
+age = int(input('Enter your age'))
+if name == 'Advika'or name == 'Ajeesh' and age >= 20 or age < 6 :
+    print('Wow you are very young')
+else :
+    print('You are very old') 
