f59dd214ef01d47d1d1927315deb82b427df3c3a
Advika <advika-vk@outlook.com>
2025-02-03 17:07:40 +0000

    swapinging numbers

---
+++ Projects/8 - Python challenger.py
@@ -0,0 +1,8 @@
+num1 = int(input('Enter a number'))
+num2 = int(input('Enter second value'))
+print('The values of num1 and num2 before swaping are  ', num1 , num2)
+temp = 0
+temp = num1
+num1 = num2
+num2 = temp
+print('The values of num1 and num2 after swaping are  ', num1 , num2)  
