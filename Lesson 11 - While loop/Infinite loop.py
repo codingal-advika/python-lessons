6e8ffa23ba6ce096595005abd6f9712a21f278c0
Advika <advika-vk@outlook.com>
2025-02-10 17:45:50 +0000

    commit infinite loop

---
+++ Lesson 11 - While loop/Infinite loop.py
@@ -0,0 +1,6 @@
+s = int(input('Enter your starting value '))
+e = int(input('Enter your ending value '))
+
+while s <= e :
+    print(s)
+   # s = s + 1
---
+++ Lesson 11 - While loop/While loop.py
@@ -0,0 +1,7 @@
+end_num = int(input('Enter your ending number '))
+start_num = int(input('Enter your starting number '))
+sum = 0
+while start_num <= end_num :
+    sum = sum + start_num
+    start_num = start_num + 1
+print('Sum = ' , sum)
