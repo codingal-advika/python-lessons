621ab9405fb82d046f9fb0861a978b87efe69034
Advika <advika-vk@outlook.com>
2025-02-10 20:30:06 +0000

    commiting nested conditional statments

---
+++ Projects/9 - Nested conditional statments.py
@@ -0,0 +1,6 @@
+print('Are you allowed to go to the pub?Let us check.')
+A = int(input('How old are you '))
+if A > 20 :
+    print('No worries you can go to the pub hassle free')
+else :
+    print('Sorry but you are leagally not allowed to enter any pub.')
