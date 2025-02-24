3d2712af140a5568ab1f14f0c33043848c9bc64c
Advika <advika-vk@outlook.com>
2025-02-24 17:44:45 +0000

    commiting functions

---
+++ lesson 15 - Functions/Functions.py
@@ -0,0 +1,14 @@
+#defining a function
+def wellwishes():
+    print('Hello! How are you?')
+
+#calling a function
+wellwishes()
+
+
+#Example program
+
+def sunny() :
+    print('It is very sunny today')
+sunny()
+
---
+++ lesson 15 - Functions/functions with parameters.py
@@ -0,0 +1,18 @@
+def student_details(name,age) :
+    print('Your name is ' , name, ' and your age is ', age)
+
+student_details('Advika' , 11)
+
+
+
+
+#activity 2
+
+def weather_condition() :
+    print('The weather is ' ,cold )
+    print('The weather in autumn is also ' , windy)
+cold = 'windy and cold'
+windy = cold
+weather_condition()
+
+
