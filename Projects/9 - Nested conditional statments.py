3228210e5c50c58155a09cc5946f4557fe6c8a1b
Advika <advika-vk@outlook.com>
2025-03-02 13:22:34 +0000

    comitting nested conditional statments

--- Projects/9 - Nested conditional statments.py
+++ Projects/9 - Nested conditional statments.py
@@ -1,6 +1,11 @@
-print('Are you allowed to go to the pub?Let us check.')
+print('Lets check if your age is between 10 or 20')
 A = int(input('How old are you '))
-if A > 20 :
-    print('No worries you can go to the pub hassle free')
-else :
-    print('Sorry but you are leagally not allowed to enter any pub.')
+if A < 0 :
+    print('Your number is a negative number.Try again')
+else:
+    if A >= 10 and A <= 20 :
+     print('You are between the age of 10 and 20')
+    else :
+     print('You are not between the age of 10 and 20')
+
+
