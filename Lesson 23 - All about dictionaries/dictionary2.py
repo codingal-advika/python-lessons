0d1fed9f14220d8d5e7fcd372353b9b8231a1aec
Advika <advika-vk@outlook.com>
2025-03-31 18:37:37 +0100

    commiting dict2

---
+++ Lesson 23 - All about dictionaries/dictionary2.py
@@ -0,0 +1,8 @@
+x = {'codingal' : 2 , 'is' : 2 , ' one of the' : 2 , 'best' : 3 , 'coding' : 4 , 'platform' : 6}
+print(x)
+k = 2
+r = 0
+for i in x:
+    if x[i] == k :
+        r = r + 1
+print(r)
