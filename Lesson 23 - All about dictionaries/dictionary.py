c5382cc0ec1d99f21409205dde0e88a464f37956
Advika <advika-vk@outlook.com>
2025-03-31 18:40:54 +0100

    committing dict2

--- Lesson 23 - All about dictionaries/dictionary.py
+++ Lesson 23 - All about dictionaries/dictionary.py
@@ -1,5 +1,6 @@
 student_data = {'id1' : '1'  , 'name' : 'Anayah' , 'grade' : '6'}
 print('The student details are ' , student_data)
 student_data ['Country'] = 'United Kingdom'
+print(student_data . get('name' , 'not found'))
 print('The student details are ' , student_data)
 print(len(student_data))
