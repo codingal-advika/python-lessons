dc373406fa2d2459691398c55e19c2a819cc2ce1
Advika <advika-vk@outlook.com>
2025-04-08 11:37:40 +0100

    committing symmetric differences

---
+++ Projects/25 - Symmetric Differenc.py
@@ -0,0 +1,5 @@
+set1 = {1 , 2 , 3 , 4 , 5 , 6}
+set2 = {1 , 5 , 6 , 7 , 8 , 9}
+symmetric_difference = set1.symmetric_difference(set2)
+print('This will print out both sets of numbers in one line')
+print('The new set off numbers are: ' , symmetric_difference)
