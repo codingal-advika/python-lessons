ee10077a2021d97ba7aade5739a32b2f8063fd24
Advika <advika-vk@outlook.com>
2025-04-22 18:06:37 +0100

    committing class

---
+++ Projects/29 - Dog Breed.py
@@ -0,0 +1,15 @@
+class Dog:
+    def __init__(self, breed, color, size , attitude):
+        self.breed = breed
+        self.color = color
+        self.size = size
+    def show_details(self):
+        print(f"Breed: {self.breed}")
+        print(f"Color: {self.color}")
+        print(f"Size: {self.size}")
+        print("-" * 30)
+dog1 = Dog("Pomeranian", "White", "Small"  ,)
+dog2 = Dog("Golden Retriever", "Golden", "Large")
+
+dog1.show_details()
+dog2.show_details()
